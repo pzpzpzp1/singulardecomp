b0fe355ea911eacf4f54b7dbd0aa37c94db1276f
Paul Z <pzpzpzp1@gmail.com>
2021-09-22 21:01:44 -0400

    add better smoothener

--- decompose_hmesh.m
+++ decompose_hmesh.m
@@ -1,4 +1,4 @@
-function out = decompose_hmesh(V,H,visualize)
+function out = decompose_hmesh(V0,H0,visualize)
     close all;  
     if nargin==0
 %         file_name = 'results_fmincon/hex_ellipsoid_coarse.vtk';
@@ -18,17 +18,27 @@ function out = decompose_hmesh(V,H,visualize)
         
         mesh = load_vtk(file_name);
 %         mesh = ImportHexMesh(file_name);
-        V = mesh.points;
-        H = mesh.cells;
+        V0 = mesh.points;
+        H0 = mesh.cells;
         visualize = 1;
     end
-
-    %% load mesh. And preprocess with padding
+    
+    %% load mesh. And preprocess with padding. extract trimesh boundary
+    [dname,fname,ext]=fileparts(file_name);
+    V=V0;H=H0;
     data = processhmesh(V,H,0);
     if (any(data.isSingularNode & data.isBoundaryVertex) && false) || contains(file_name,'unit.vtk')
         [V,H] = padhmesh(V,H);
     end
     data = processhmesh(V,H,visualize);
+    % boundary triangle mesh for projection
+    trimesh0.Vertices = data.V;
+    trimesh0.Faces = [data.F(data.isBoundaryFace,[1 2 3]);  data.F(data.isBoundaryFace,[3 4 1])];
+    [trimesh0.Vertices, trimesh0.Faces] = minimizeMesh(trimesh0.Vertices, trimesh0.Faces);
+    % save starting mesh as index 0
+    outname = sprintf('results/%s_0.vtk',fname);
+    mesh.points = V; mesh.cells = H;
+    save_vtk(mesh, outname)
     
     %% Begin decomposition
     Vs{1} = V; Hs{1} = H;
@@ -36,7 +46,8 @@ function out = decompose_hmesh(V,H,visualize)
     while any(data.isSingularNode & ~data.isBoundaryVertex)
         %% choose random node to simplify
         singularNodes = find(data.isSingularNode & ~data.isBoundaryVertex);
-        selind = randi(numel(singularNodes));
+        selind = randi(numel(singularNodes)); 
+%         selind = 1;
         node_ind = singularNodes(selind);
         
         %% build map from singular node to T(S2)
@@ -53,23 +64,31 @@ function out = decompose_hmesh(V,H,visualize)
         [V,H]=sheetinsertion(data, cut);
         
         %% geometric simplification
-        [V,H] = smoothenhmesh(V,H);
-        
+        V = smoothenhmesh(V,H,trimesh0,visualize);
+%{
+          mesh.points = V; mesh.cells = H;
+          new_mesh = repair_mesh(mesh)
+          V=mesh.points; H=mesh.cells;
+%}
         %% recompute data
         data = processhmesh(V,H,visualize); title(num2str(iter));
         Vs{iter} = V; Hs{iter} = H; iter = iter+1;
+        outname = sprintf('results/%s_%d.vtk',fname,iter); mesh.points = V; mesh.cells = H; save_vtk(mesh, outname);
     end
     
     out.Vs=Vs;
     out.Hs=Hs;
-    
-    [dname,fname,ext]=fileparts(file_name);
-    outname = sprintf('results/%s.vtk',fname);
-    mesh.points = V; mesh.cells = H;
-    save_vtk(mesh, outname)
-    
 end
+%{
+V=out.Vs{end}; H=out.Hs{end};
+data = processhmesh(V,H,1);
 
-
+figure; hold all; axis equal off; rotate3d on;
+patch('vertices',data.V,'faces',data.F(:,:),'facecolor','green','facealpha',.1,'edgealpha',0)
+patch('vertices',V,'faces',E(isBoundaryEdge,[1 2 1]))
+patch('vertices',V,'faces',E(isSingularEdge,[1 2 1]),'linewidth',3,'edgecolor','blue')
+scatter3(V(isSingularNode,1),V(isSingularNode,2),V(isSingularNode,3),100,'r','filled')
+       
+%}
 
 
--- smoothenhmesh.m
+++ smoothenhmesh.m
@@ -1,21 +1,99 @@
-function [V,H] = smoothenhmesh(V0,H)
+% minimizes dirichlet energy for interior vertices to get smooth equi-edge length mesh
+% also minimizes scaled jacobian to ensure no hex element gets horribly skewed compared to any other
+% DOES NOT PRESERVE BOUNDARY AT ALL. used for topological analysis. boundary isn't much of a concern.
+function [V, out] = smoothenhmesh(V0, H, trimesh, visualize)
+    if visualize
+        figure; hold all; axis equal; rotate3d on; axis off;
+        F = hex2face(H);
+    end
+    data = processhmesh(V0,H,0); L = data.graphlaplacian;
+        
+    V=V0; p=1; 
+    E = hex2edge(H);
+    elens = vecnorm(V(E(:,1),:)-V(E(:,2),:),2,2);
+    dt = min(elens)/300;
+    lfac = 500;
+    for i=1:500
+        Vs{i}=V;
+        if visualize
+            try; delete(ptc); catch; end;
+            ptc = patch('vertices',V,'faces',F,'facealpha',.1,'facecolor','green');
+            drawnow;
+        end
+        
+        [Esj, grad_sj, out] = scaledJacobian_hmesh(V,H,p);
+        [Elap, grad_lap] = dirE(L, V); grad_lap(data.isBoundaryVertex,:) = 0;
+        energy(i) = Esj + lfac*Elap;
+        grad = grad_sj + lfac*grad_lap;
+        
+        % line search. max 50 iterations to get ls to work.
+        for j=1:50
+            candV = V-dt*grad;
+            esj = scaledJacobian_hmesh(candV,H,p);
+            elap = dirE(L, candV);
+            etot = esj + lfac*elap;
+            if etot > energy(i)
+                dt = dt/2;
+            else
+                break
+            end
+        end
+        dt = dt*(2^(1/4));
+        dts(i) = dt;
+        
+        % update V
+        V = V - dt*grad;
+    end
+    out.Vs = Vs;
+    out.energy = energy;
+    out.dts = dts;
     
-    V=V0;
-    data = processhmesh(V,H,1);
+    
+    
+    
+    
+    
+    
+    
+    
+    %{
+    c0 = (max(V0)+min(V0))/2; 
+    V0 = V0 - c0;
+    BBd0 = norm([max(V0)-min(V0);]);
+    trimesh.Vertices = trimesh.Vertices - c0;
+    
+    V=V0; 
+    data = processhmesh(V,H,0);
     L = data.graphlaplacian;
     
     figure; hold all; rotate3d on; axis equal off;
-    onorm = norm(V,'fro');
-    for i=1:100
-        V=V-.01*L*V;
-        V=V/norm(V,'fro');
-        V=V*onorm;
+    ptc1 = patch('faces',trimesh.Faces,'vertices',trimesh.Vertices,'facealpha',.1,'facecolor','red','edgealpha',0);
+    for i=1:10
+        V = (speye(data.nV) + .1*L)\V;
+        energy(end+1) = norm((L*V).*V,'fro')^2/2
+    
+        % recenter rescale
+        V=V- [max(V)+min(V)]/2;
+        BBd = norm([max(V)-min(V)]);
+        V=V/BBd*BBd0;
+        
+%         project boundary back to boundary
+%         [dist, projpts, faces2, vertices2, corresponding_vertices_ID, new_faces_ID] = point2trimesh('Faces',trimesh.Faces,'Vertices',trimesh.Vertices,'QueryPoints',V(data.isBoundaryVertex,:));
+%         V(data.isBoundaryVertex,:) = projpts;
+        V(data.isBoundaryVertex,:) = V0(data.isBoundaryVertex,:);
         
         try; delete(ptc); catch; end;
-        ptc = patch('faces',data.F,'vertices',V,'facealpha',.1,'facecolor','green')
+        ptc = patch('faces',data.F,'vertices',V,'facealpha',.1,'facecolor','green');
         drawnow;
     end
-    
+    %}
     
 
+end
+
+
+function [energy,grad] = dirE(L, V)
+    grad = (L*V);
+    energy = sum(grad.*V,[1 2])/2;
+    
 end
